17c62503804870edffef1dc63ef2c03d6a228fa5
Alfredo Guevara Fuentes <aguevara24@hotmail.com>
2024-06-29 18:30:07 -0600

    Fin Sección 27

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import * as process from 'process';
   imports: [
     ConfigModule.forRoot(),
 
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     AuthModule,
   ],
   controllers: [],
